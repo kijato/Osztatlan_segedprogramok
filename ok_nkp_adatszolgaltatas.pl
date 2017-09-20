a6798b21ac680d5a885c643a02684deb5727c137
kijato <kijato@gmail.com>
2017-09-20 11:22:44 +0200

    Add files via upload

---
+++ ok_nkp_adatszolgaltatas.pl
@@ -0,0 +1,179 @@
+#!/usr/bin/perl
+#
+#	Copyright (c) Kis János Tamás, 2015. január 22.
+#	VERZIOSZAM ='1.13 (2017. február 15.)'
+#
+
+use strict;
+#use warnings;
+#use Encoding;
+#use Data::Dumper;
+#use locale; # Ez kell(het) az ABC szerinti helyes rendezéshez.
+$| = 1; # STDOUT buffer out;
+
+#if ( @ARGV<1 ) { die "Hibás paraméterezés...!" }
+if ( @ARGV!=1 ) { die <<END
+
+A program a TAKAROS-Osztatlan moduljából származó, NKP-ra szûrt "vállalkozói"
+adatszolgáltatás (azaz "név szerinti lekérdezés") eredményébõl elõállítja az
+NKP igénye szerinti szerkezetû adatokat.
+Aa betöltés közben a kérelmezõkre, tulajdonosokra és területi adatokra
+vonatkozóan történik "némi" konzisztencia-vizsgálat is, melynek eredménye
+szintén bekerül a naplóként funkcionáló, LOG kiterjesztésû állományba.
+
+(Az ellenõrzés az "idõkövetéses" lekérdezés feldolgozása esetén nem lesz
+megfelelõ, mert annak struktúrája eltér az "Adatszolgáltatás"-sal elõállítható
+fájlok struktúrájától.)
+
+Rendszer-követelmények:
+------------------------
+- Minimális Perl környezet;
++ szükség esetén a Data::Dumper modul (a részletes naplózás miatt).
+
+Használat:
+----------
+[perl.exe] $0 adatfile.csv
+
+Kimenet:
+--------
+1.
+Egy 'Helyrajziszámok.csv', mely tartalmazza a szükéges formátumú hrsz felsorolást,
+illetve 1-1-1 'Kerelmezok', 'Tulajdonosok', és 'Visszamaradok' könyvtár, melyek
+hrsz-enként tartalmazzák a könyvtárnév szerinti szûrt adatokat.
+2.
+A paraméterként kapott fájlnév+'.log' fájl tartalmazza a valamilyen szempontból
+'szokatlan' tételeket, mint például:
+- megszûnt eljárások és ezekhez tartozó adatsorok;
+- hibásnak vélt adatok a kérelmezõk, a tulajdonosok, illetve a területi adatok vonatkozásában.
+
+END
+}
+
+my $riport_file = my $log_file = shift(@ARGV);
+unless (-f $riport_file) { die "\nA(z) '$riport_file' nem létezik...!\n"; }
+my ($fajlnev,$hrszsorok)='';
+my ($sorszam,$ervenyes_sor,$ervenyes_sorok,$ervenytelen_sorok)=0;
+my %adatok;
+
+$log_file=~s/(\.\w*)$//;
+open LOG,">$log_file.log" || die "$!\n";
+print LOG "A feldolgozás kezdete: ".`date`."\n";
+
+open FH,$riport_file || die "$!\n";
+print "\nA feldolgozás indul...\n";
+while(<FH>) {
+	chomp;
+	print "\tFeldolgozott sorok száma: ".++$sorszam."\tÉrvényes HRSZ-ek száma: ".$ervenyes_sorok."\tÉrvénytelen HRSZ-ek száma: ".$ervenytelen_sorok.".\r";
+	my @sor = split(/;/);
+	if ( $sor[0] == 1 && $sor[17] == 1) {
+		$ervenyes_sor=1;
+		$ervenyes_sorok++;
+		$fajlnev = $sor[2]."_".$sor[3]."_".$sor[4]."_".$sor[5].".csv";
+		$hrszsorok .= $sor[1].";".$sor[2].";".$sor[3].";".$sor[4].";".$sor[5].";".$sor[6].";".$sor[9].";".$sor[8].";".$fajlnev.";".$sor[10].";".$sor[11].";".$sor[12].";".$sor[15].";\n";
+
+		if ( $sor[7] > $sor[6] ) { print LOG "\n\n$sor[2], $sor[3], $sor[4]/$sor[5] esetében nagyobb a kiosztandó terület, mint az ingatlan teljes területe!\n\n"; }
+		if ( $sor[8] > $sor[9] ) { print LOG "\n\n$sor[2], $sor[3], $sor[4]/$sor[5] esetében nagyobb a kérelmezõk száma, mint a tulajdonosok száma!\n\n"; }
+		if ( $sor[8] == 0 || $sor[8] == '' || !defined($sor[8]) )  { print LOG "\n\n$sor[2], $sor[3], $sor[4]/$sor[5] esetében a kérelmezõk száma: '$sor[8]'!\n\n"; }
+		if ( $sor[9] == 1 || $sor[9] == '' || !defined($sor[9]) )  { print LOG "\n\n$sor[2], $sor[3], $sor[4]/$sor[5] esetében a tulajdonosok száma: '$sor[9]'!\n\n"; }
+
+		$adatok{$fajlnev} = {'Kerelmezok'=>'', 'Tulajdonosok'=>'', 'Visszamaradok'=>''};
+	} elsif ( $sor[0] == 1 && $sor[17] != 1) {
+		$ervenyes_sor=0;
+		$ervenytelen_sorok++;
+		print LOG "\nA(z) $sorszam. sort a státuszkód ($sor[17]) miatt kihagyom ($sor[2]_$sor[3]_$sor[4]_$sor[5])!\n";
+	} elsif ( $ervenyes_sor && $sor[0] == 2 ) {
+		$adatok{$fajlnev}{'Kerelmezok'} .= $sor[1].";".";".";".";".$sor[3].";".$sor[4].";\n";
+	} elsif ( $ervenyes_sor && $sor[0] == 3 ) {
+		$adatok{$fajlnev}{'Tulajdonosok'} .= $sor[1].";".";".";".";".$sor[3].";".$sor[4].";\n";
+	} elsif ( $ervenyes_sor && $sor[0] == 4 ) {
+		$adatok{$fajlnev}{'Visszamaradok'} .= $sor[1].";".";".";".";".$sor[3].";".$sor[4].";\n";
+	} elsif ( !$ervenyes_sor ) {
+		print LOG "A(z) $sorszam. sort 'földrészlet' státusz-kódja miatt kihagyom: ($sor[1] [$sor[3]/$sor[4]])\n";
+	#} elsif (/^\D+/) {
+	#	next; # A fejléc sor csendes átlépése... sorszámozás...?
+	} else {
+		die "Ez meg ($sorszam.) miféle sor lehet...?";
+	}
+}
+close FH;
+print "\nA feldolgozás kész!\n\n";
+
+#print Dumper(%adatok);
+
+print "A kiírás indul...\n";
+open FH,">Helyrajziszamok.csv";
+print FH "Települési sorszám;Település név;Fekvés;Helyrajziszám;Alátörés;Érintett földrészletek területe;Tulajdonosok száma;Kérelmet benyújtók száma;Kérelmezõnkénti összesített tulajdoni hányad;Állami tulajdon megjelölése;Perfeljegyzés darabszáma;Terhek darabszáma;Széljegyek darabszáma;\n";
+print FH $hrszsorok;
+close FH;
+
+my $i=0;
+foreach my $fajlnev (keys %adatok) {
+	foreach my $tipus (keys %{$adatok{$fajlnev}}) {
+		unless (-d $tipus) { mkdir $tipus; }
+		if( !defined(${$adatok{$fajlnev}}{$tipus}) || length(${$adatok{$fajlnev}}{$tipus})==0 ) {
+			print LOG "\nA(z) '$tipus\\$fajlnev' nem tartalmazna adatot, ezért nem hozom létre!\n";
+		} else {
+			open FH, ">$tipus\\$fajlnev";
+			print FH "Név;Születési év;Anyja neve;Lakcím;Kérelmezõnkénti összesített tulajdoni hányad számláló;Kérelmezõnkénti összesített tulajdoni hányad nevezõ;\n";
+			print FH ${$adatok{$fajlnev}}{$tipus};
+
+			#my @sorok = split(/^/m,${$adatok{$fajlnev}}{$tipus});
+			#my $szemelyek={};
+			#foreach(@sorok) {
+			#    my @sor=split(/;/);
+			#    $szemelyek->{$sor[0]} += $sor[1];
+			#    $szemelyek->{'nevezo'} = chomp($sor[2]);
+			#}
+			## print LOG Dumper($szemelyek);
+			#if($szemelyek->{$sor[0]} >= $szemelyek->{'nevezo'}) {
+			#    print LOG "\n\nhiba a $tipus\\$fajlnev' fájlban:".$szemelyek->{$sor[0]}.' >= '.$szemelyek->{'nevezo'};
+			#}
+			#    if ( ( $sor[0] == 2 || $sor[0] == 3 ) && $sor[3]==$sor[4] ) 
+
+			close FH;
+		}
+	}
+	print "\tA feldolgozott HRSZ-ek száma: ".++$i."/".(keys %adatok)."\r";
+}
+print "\nA kiírás kész!\n";
+
+print LOG "\n\nA feldolgozás vége: ".`date`;
+close LOG;
+
+#print "A folytatáshoz nyomj ENTER-t...!";
+#<>;
+
+exit;
+
+__END__
+
+#
+#   A program szabad szoftver, bárki továbbadhatja és/vagy módosíthatja
+#   a legfrissebb GNU General Public Licence feltételei szerint, ahogy
+#   azt a Free Software Foundation közli. A program módosított változatai,
+#   a változatlan másolatokkal megegyezô feltételek alapján másolhatók
+#   és terjeszthetôk, ha a módosított változatot is, az ezzel az enge-
+#   déllyel megegyezô feltételek szerint terjesztik. A lefordított kód
+#   is a módosított változat kategóriájába tartozik. Az üzleti célú
+#   terjesztés NEM MEGENGEDETT. Módosítás és terjesztés elôtt a dolgok
+#   naprakészségének biztosítása végett ajánlott kapcsolatba lépni a
+#   szerzôvel. A program módosítása esetén, kérlek juttatsd el a szerzô-
+#   nek a módosított forráskódot valamilyen formában, hogy a hasznos
+#   változás mihamarabb bekerüljön a szoftver "hivatalos" disztribúció-
+#   jába. A nyomtatott változat jobban néz ki...   :-)
+#   E programot (én, a szerzô) azzal a reménnyel terjesztem, hogy hasznos
+#   lesz, de MINDENFÉLE GARANCIA VÁLLALÁSA NÉLKÜL. A részletek a GNU
+#   General Public Licence-ben találhatók. A programmal együtt kapnod
+#   kellett egy másolatot a GNU General Public Licence-bôl, ha nem kaptál,
+#   írj a következô címre:
+#   Free Software Foundation, Inc. 675 Mass Ave, Cambridge, MA 02139, USA
+#   A program szerzôje a következô címeken érhetô el :
+#   ×----------------------------------------------------------------×
+#   |  Kis János Tamás, E-mail: kjt@takarnet.hu, kijato@gmail.com    |
+#   |                   Tel.: (76) 502-562                           |
+#   ×----------------------------------------------------------------×
+#   *================================================================×
+#   |       A PROGRAM SZABADON TERJESZTHETÖ, HA A COPYRIGHT ÉS       |
+#   |         AZ ENGEDÉLY SZÖVEGÉT MINDEN MÁSOLATON MEGÕRZIK.        |
+#   ×================================================================*
+#
---
+++ ok_posta.pl
@@ -0,0 +1,147 @@
+#!/usr/bin/perl
+#
+#	Copyright (c) Kis János Tamás, 2017. február 10.
+#	VERZIOSZAM ='0.32 (2017. április 25.)'
+#
+
+use strict;
+#use warnings;
+#use Encoding;
+use Data::Dumper;
+#use locale; # Ez kell(het) az ABC szerinti helyes rendezéshez.
+$| = 1; # STDOUT buffer out;
+
+#if ( @ARGV<1 ) { die "Hibás paraméterezés...!" }
+if ( @ARGV!=1 ) { die <<END
+
+A program a TAKAROS-Osztatlan moduljából származó, NKP-ra szûrt "vállalkozói"
+adatszolgáltatás (azaz "név szerinti lekérdezés") eredményébõl elõállítja a
+körlevelekhez felhasználható adatforrást.
+
+Rendszer-követelmények:
+------------------------
+- Minimális Perl környezet;
+
+Használat:
+----------
+[perl.exe] $0 adatfile.csv
+
+Kimenet:
+--------
+Egy olyan CSV fájl, mely alkalmas a körlevelek adatforrásaként történõ
+felhasználásra. A kimeneti fájl neve a bemeneti fájl nevébõl képzõdik,
+a fájlnév '_$0' karakterekkel történõ kiegészítésével.
+
+END
+}
+
+my $riport_file = shift(@ARGV);
+unless (-f $riport_file) { die "\nA(z) '$riport_file' nem létezik...!\n"; }
+my ($sorszam,$ervenyes_hrsz,$ervenyes_hrszok,$ervenytelen_hrszok)=0;
+my ($hrsz,$nev_cim)='';
+my %adatok;
+
+open FH,$riport_file;
+
+print "A beolvasás indul: ".`time /t`."\n";
+while(<FH>) {
+	chomp;
+	print "\tBeolvasott sorok száma: ".++$sorszam."\tétvényes HRSZ-ek száma: ".$ervenyes_hrszok."\térvénytelen HRSZ-ek száma: ".$ervenytelen_hrszok.".\r";
+	my @sor = split(/;/);
+	if($sor[0] == 1 && $sor[17] == 1) { # Ez a 'hrsz' sora és a státusz '1', azaz 'el nem indított eljárás'-ról van szó.
+		$ervenyes_hrsz=1;
+		$ervenyes_hrszok++;
+		if($sor[3] =~ /^k/) {	$sor[4]="0".$sor[4]; } # Ha a fekvés 'k'-val kezdõdik, akkor a hrsz elé beszúrunk egy '0'-át.
+		if($sor[5] eq '') {
+			$hrsz = $sor[2].";".$sor[3].";".$sor[4]; # Nincs alátörés a hrsz-ban...
+		} else {
+			$hrsz = $sor[2].";".$sor[3].";".$sor[4]."/".$sor[5]; # ...van alátörés a hrsz-ban.
+		}
+	} elsif ( $sor[0] == 1 && $sor[17] != 1)   {	$ervenyes_hrsz=0; $ervenytelen_hrszok++; $nev_cim=''; next; # Ez a 'hrsz' sora, de a státusz nem 'el nem indított'
+	} elsif ( $ervenyes_hrsz && $sor[0] == 2 ) {	next; # A '2'-essel kezdõdõ sorok a kérelmezõk adatait tartalmazzák.
+	} elsif ( $ervenyes_hrsz && $sor[0] == 3 ) {	$nev_cim=$sor[1].";".$sor[2]; # A '3'-assal kezdõdõ sorok a tulajdonosok adatait tartalmazzák.
+	} elsif ( $ervenyes_hrsz && $sor[0] == 4 ) {	next; # A '4'-essel kezdõdõ sorok a nem kérelmezõk (visszamaradók) adatait tartalmazzák.
+	} elsif ( !$ervenyes_hrsz )                {	next; # Ha a hrsz sora 'érvénytelen', új sort olvasunk be az adat-fájlból.
+	} else                                     {	die "Ez meg miféle sor lehet...? ($sorszam.)"; # Ilyen eset elvileg nem lehet...
+	}
+	# push @{$adatok{$hrsz}},$nev_cim;
+	push @{$adatok{$nev_cim}},$hrsz; # A 'nev_cim' kulcshoz tartozó tömbhöz hozzáadjuk a hrsz-t.
+	$nev_cim=''; # Kinullázzuk a 'nev_cim'-et, hogy a következõ sornál biztosan ne okozzon galibát...
+}
+close FH;
+print "\n\nA beolvasás kész: ".`time /t`;
+
+#print Dumper \%adatok;
+
+print "\nA feldolgozás indul: ".`time /t`;
+foreach my $kulcs ( keys %adatok ) { # Végigmegyünk a hash-en: fogjuk az egyik kulcsot...
+	my %adat;
+	foreach my $a ( @{$adatok{$kulcs}} ) { # ... végigmegyünk a kulccsal azonosított tömb elemein...
+		$adat{$a}++; # ... megszámoljuk a tömb elemeinek elõfordulásait. (Ebbõl több is lehet, mivel egy hrsz-en belül, egy embernek több bejegyzése is lehet.)
+	}
+	$adatok{$kulcs}= \%adat; # Az így készített HASH-sel felülírjuk az eredeti hash-nek az adott kulcshoz tartozó TÖMB-jét! (Azaz itt történik a 'varázslat', ami a többszörös hrsz-ekbõl egyet, pontosabban egy kulcsot csinál és mellékesen a kulcshoz tartozó érték az elõfordulások számát adja vissza.)
+}
+print "\nA feldolgozás kész: ".`time /t`;
+
+#print Dumper \%adatok;
+
+print "\nA kiírás indul: ".`time /t`;
+$riport_file=~s/(\.\w*)$//; # Töröljük a kiterjesztést, a '.'-tal együtt. (A '$1' tárolja az eredeti kiterjesztést...)
+open FH,">$riport_file.$0$1" || die "$!\n"; # A szkript aktuális nevét ($0) és az eredeti kiterjesztést ($1) hozzáfûzzük a fájlnévhez.
+binmode FH; # Ez biztosítja a 'nyers' írást a fájlba, enélkül a vezérlõ karakterek nem minden esetben értelmezõdnek megfelelõen.
+print FH "név;cím;hrszek".chr(13).chr(10);
+foreach my $kulcs ( sort keys %adatok ) { # Végigmegyünk a rendezett kulcsokkal a hash-en...
+	#if($kulcs=~/.*;.*;.*/) {next;}
+	if($kulcs eq '' || !defined($kulcs)){next;} # .. ha nincs kulcs, vagy 'üres', akkor 'ugrunk'...
+
+	#my $sor="$kulcs;\"";
+	
+	# Kulcs minta: "vezetéknév keresztnév;irsz település utca házszám."
+	#               $1                    $2   $3        $4
+	my $sor="$kulcs";
+	$sor=~/(.+);(\d+)\s+(\w+)\s+(.+)/;
+	$sor="$1;\"$3".chr(10).$4.chr(10)."$2\";\"";
+
+	foreach my $a ( sort keys %{$adatok{$kulcs}} ) {
+		$a=~s/;/ /g;
+		$sor.=$a.chr(10);
+	}
+	chop($sor);
+	print FH "$sor\"".chr(13).chr(10);
+}
+close FH;
+print "\nA kiírás kész: ".`time /t`;
+
+
+__END__
+
+#
+#   A program szabad szoftver, bárki továbbadhatja és/vagy módosíthatja
+#   a legfrissebb GNU General Public Licence feltételei szerint, ahogy
+#   azt a Free Software Foundation közli. A program módosított változatai,
+#   a változatlan másolatokkal megegyezô feltételek alapján másolhatók
+#   és terjeszthetôk, ha a módosított változatot is, az ezzel az enge-
+#   déllyel megegyezô feltételek szerint terjesztik. A lefordított kód
+#   is a módosított változat kategóriájába tartozik. Az üzleti célú
+#   terjesztés NEM MEGENGEDETT. Módosítás és terjesztés elôtt a dolgok
+#   naprakészségének biztosítása végett ajánlott kapcsolatba lépni a
+#   szerzôvel. A program módosítása esetén, kérlek juttatsd el a szerzô-
+#   nek a módosított forráskódot valamilyen formában, hogy a hasznos
+#   változás mihamarabb bekerüljön a szoftver "hivatalos" disztribúció-
+#   jába. A nyomtatott változat jobban néz ki...   :-)
+#   E programot (én, a szerzô) azzal a reménnyel terjesztem, hogy hasznos
+#   lesz, de MINDENFÉLE GARANCIA VÁLLALÁSA NÉLKÜL. A részletek a GNU
+#   General Public Licence-ben találhatók. A programmal együtt kapnod
+#   kellett egy másolatot a GNU General Public Licence-bôl, ha nem kaptál,
+#   írj a következô címre:
+#   Free Software Foundation, Inc. 675 Mass Ave, Cambridge, MA 02139, USA
+#   A program szerzôje a következô címeken érhetô el :
+#   ×----------------------------------------------------------------×
+#   |  Kis János Tamás, E-mail: kijato@gmail.com, kjt@takarnet.hu    |
+#   |                   Tel.: (76) 795-810                           |
+#   ×----------------------------------------------------------------×
+#   *================================================================×
+#   |       A PROGRAM SZABADON TERJESZTHETÖ, HA A COPYRIGHT ÉS       |
+#   |         AZ ENGEDÉLY SZÖVEGÉT MINDEN MÁSOLATON MEGÕRZIK.        |
+#   ×================================================================*
+# 
